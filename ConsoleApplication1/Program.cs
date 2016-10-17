6ad2294795b8e3dd11a744f8373c0cbc3dd2ea7b
Florian <4stiefel@informatik.uni-hamburg.de>
2016-10-17 09:39:55 +0200

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Cyclist cyclist = null;
+            cyclist?.ring();
+
+            cyclist = new Cyclist();
+            cyclist.ring();
+            Console.Read();
+           
+        }
+    }
+}
